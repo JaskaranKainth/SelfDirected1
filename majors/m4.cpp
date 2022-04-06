98fb97f243b28aba376c6833ecd20ac0d20f6443
JaskaranKainth <Jaskarankainth@hotmail.com>
2022-04-06 18:05:36 -0400

    Toronto Raptors Team added with scores

--- majors/m4.cpp
+++ majors/m4.cpp
@@ -11,4 +11,9 @@ int main(){
         teamsText << "Brooklyn Nets.txt \n";
         teamsText << "Phoenix Suns.txt \n";
         teamsText << "Denver Nuggets.txt \n";
+
+            ofstream torText; torText.open("Toronto Raptors.txt");
+            torText << "Miami Heat, 109-114 \n";
+            torText << "Orlando Magic, 102-89 \n";
+            torText << "Minnesota Timberwolves, 125-102 \n";
 }
