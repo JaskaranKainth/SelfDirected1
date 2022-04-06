5902fc47a1c52f78ef2f6555f96176ad86d03e3c
JaskaranKainth <Jaskarankainth@hotmail.com>
2022-04-06 18:08:30 -0400

    Last team denver nuggets added with scores

--- majors/m4.cpp
+++ majors/m4.cpp
@@ -12,19 +12,23 @@ int main(){
         teamsText << "Phoenix Suns.txt \n";
         teamsText << "Denver Nuggets.txt \n";
 
-            ofstream torText; torText.open("Toronto Raptors.txt");
+        ofstream torText; torText.open("Toronto Raptors.txt");
             torText << "Miami Heat, 109-114 \n";
             torText << "Orlando Magic, 102-89 \n";
             torText << "Minnesota Timberwolves, 125-102 \n";
 
-            ofstream bklynText; bklynText.open("Brooklyn Nets.txt");
+        ofstream bklynText; bklynText.open("Brooklyn Nets.txt");
             bklynText << "Miami Heat, 110-95";
             bklynText << "Detroit Pistons, 130-123";
             bklynText << "Atlanta Hawks, 115-122";
 
-            ofstream phxText; phxText.open("Phoenix Suns.txt");
+        ofstream phxText; phxText.open("Phoenix Suns.txt");
             phxText << "Oklahoma Thunder, 96-117";
             phxText << "Golden State Warriors, 107-103";
             phxText << "Denver Nuggets, 140-130";
 
+        ofstream denText; denText.open("Denver Nuggets.txt");
+            denText << "Los Angeles Lakers, 129-118";
+            denText << "Indiana Pacers, 125-118";
+            denText << "Oklahoma Thunder, 113-107";
 }
